65e28d30e9416f973dec8605f8c10e50911c1ed0
Alejandro <alejandro.gonzalez.coene@gmail.com>
2026-04-17 22:20:21 -0300

    Generacion de los Modelos del Proyecto

--- core/GesAcad/models.py
+++ core/GesAcad/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Perfiles(models.Model):
+    id_perfil = models.AutoField(primary_key=True)
+    descripcion = models.CharField()
+
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField()
+    apellido = models.CharField()
+    dni = models.CharField(unique=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField()
+    id_perfil = models.ForeignKey(Perfiles, on_delete=models.CASCADE)
+
+class Materias(models.Model):
+    id_materia = models.AutoField(primary_key=True)
+    nombre = models.CharField()
+    anio = models.IntegerField()
+    cuatrimestre = models.IntegerField()
+    
+class Examenes(models.Model):
+    id_examen = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    id_materia = models.ForeignKey(Materias, on_delete=models.CASCADE)
+
+class Carreras(models.Model):
+    id_carrera = models.AutoField(primary_key=True)
+    nombre = models.CharField()
+
+class Carrera_Materia(models.Model):
+    id_materia = models.ForeignKey(Materias, on_delete=models.CASCADE)
+    id_carrera = models.ForeignKey(Carreras, on_delete=models.CASCADE)
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["id_materia","id_carrera"], name="unique_id_materia_carrera")]
+
+class Inscripcion_Materia(models.Model):
+    id_inscripcion_materia = models.AutoField(primary_key=True)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    estado = models.IntegerField()
+    id_materia = models.ForeignKey(Materias, on_delete=models.CASCADE)
+
+class Inscripcion_Examen(models.Model):
+    id_examen = models.ForeignKey(Examenes, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    nota = models.FloatField()
+    estado = models.IntegerField()
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["id_usuario","id_examen"], name="unique_id_usuario_examen")]
